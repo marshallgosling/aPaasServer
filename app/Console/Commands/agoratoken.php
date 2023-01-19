b17c382ea9d88a3d6ff09bdfe740635e9f61ac4c
gaoqi <gaoqi@nathangao.local>
2023-01-19 11:27:25 +0800

    test

--- app/Console/Commands/agoratoken.php
+++ app/Console/Commands/agoratoken.php
@@ -47,10 +47,14 @@ class agoratoken extends Command
 
         foreach($list as $l)
         {
+            echo " ------------------------------------------------\n";
             echo $l."\n";
+            echo base64_decode($l)."\n\n";
         }
-        
-        echo "eyJzb25nX25vIjoiNjI0NjI2MjcyNzI4MTg3MCIsInNvbmdfbmFtZSI6Ilx1NWM0Ylx1OTg3NiIsInNvbmdfdXJsIjoiaHR0cHM6XC9cL2FjY2t0di5zZC1ydG4uY29tXC8yMDIyMDcxOTE0NDBcLzI1OGYxMjI3MzU2YmQxNDZmYTJkOTM3ZjJmZDVmYWE1XC9yZWxlYXNlXC8xXC8zZVwvbXAzXC83MFwvQ2htRkdscHpUYUNBT2x1OEFFM0xHbUpqQlI0NjcwLm1wMyIsImltYWdlX3VybCI6Imh0dHBzOlwvXC9hY2NwaWMuc2QtcnRuLmNvbVwvcGljXC9yZWxlYXNlXC9qcGdcLzFcLzJkMWFlZFwvQ2htRloxUzBpbW1BVnRTREFBT0RWUnpNTkhnNDU0LmpwZyIsInNpbmdlciI6Ilx1NTQ2OFx1Njc3MFx1NGYyNixcdTZlMjlcdTVjOWEiLCJseXJpYyI6Imh0dHBzOlwvXC9hY2NrdHYuc2QtcnRuLmNvbVwvMjAyMjA3MjAxNDAyXC82NzFlOTAyMGNlMTFmMTQ0YmY1Yzc5NDE3MjlhNjYwOVwvcmVsZWFzZVwvbHlyaWNcL3ppcF91dGY4XC8xXC80NTczMzEzOTQ4NDQ0ODhiYmQ5NGUwYzhmYzc4NGY3Yy56aXAiLCJzb3J0IjoiMiIsInVzZXJfbm8iOiIifQ==\n";
+        echo " sample ------------------------------------------------\n";
+        $sample = "eyJzb25nX25vIjoiNjI0NjI2MjcyNzI4MTg3MCIsInNvbmdfbmFtZSI6Ilx1NWM0Ylx1OTg3NiIsInNvbmdfdXJsIjoiaHR0cHM6XC9cL2FjY2t0di5zZC1ydG4uY29tXC8yMDIyMDcxOTE0NDBcLzI1OGYxMjI3MzU2YmQxNDZmYTJkOTM3ZjJmZDVmYWE1XC9yZWxlYXNlXC8xXC8zZVwvbXAzXC83MFwvQ2htRkdscHpUYUNBT2x1OEFFM0xHbUpqQlI0NjcwLm1wMyIsImltYWdlX3VybCI6Imh0dHBzOlwvXC9hY2NwaWMuc2QtcnRuLmNvbVwvcGljXC9yZWxlYXNlXC9qcGdcLzFcLzJkMWFlZFwvQ2htRloxUzBpbW1BVnRTREFBT0RWUnpNTkhnNDU0LmpwZyIsInNpbmdlciI6Ilx1NTQ2OFx1Njc3MFx1NGYyNixcdTZlMjlcdTVjOWEiLCJseXJpYyI6Imh0dHBzOlwvXC9hY2NrdHYuc2QtcnRuLmNvbVwvMjAyMjA3MjAxNDAyXC82NzFlOTAyMGNlMTFmMTQ0YmY1Yzc5NDE3MjlhNjYwOVwvcmVsZWFzZVwvbHlyaWNcL3ppcF91dGY4XC8xXC80NTczMzEzOTQ4NDQ0ODhiYmQ5NGUwYzhmYzc4NGY3Yy56aXAiLCJzb3J0IjoiMiIsInVzZXJfbm8iOiIifQ==\n";
+        echo $sample."\n";
+        echo base64_decode($sample);
         exit;
         
         $arr = Auction::pluck('name', 'id')->toArray();
