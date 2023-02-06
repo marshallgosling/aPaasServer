f74a3b4fabb0e9ea7a86e313cbe5c64f44d9468d
gaoqi <gaoqi@nathangao.local>
2023-02-06 14:51:30 +0800

    agora:install

---
+++ app/Console/Commands/Agora/Install.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Console\Commands\Agora;
+
+use Illuminate\Console\Command;
+use Encore\Admin\Auth\Database\Menu;
+
+class Install extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'agora:install {menu}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        
+        $id = $this->argument('menu');
+
+        $menu = Menu::where('parent_id', $id)->orderBy("order")->lazy()->toArray();
+
+        echo json_encode($menu);
+
+        return;
+        
+        Menu::insert([
+            [
+                'parent_id' => 0,
+                'order'     => 1,
+                'title'     => 'Dashboard',
+                'icon'      => 'fa-bar-chart',
+                'uri'       => '/',
+            ],
+            [
+                'parent_id' => 0,
+                'order'     => 2,
+                'title'     => 'Admin',
+                'icon'      => 'fa-tasks',
+                'uri'       => '',
+            ],
+            [
+                'parent_id' => 2,
+                'order'     => 3,
+                'title'     => 'Users',
+                'icon'      => 'fa-users',
+                'uri'       => 'auth/users',
+            ],
+            [
+                'parent_id' => 2,
+                'order'     => 4,
+                'title'     => 'Roles',
+                'icon'      => 'fa-user',
+                'uri'       => 'auth/roles',
+            ],
+            [
+                'parent_id' => 2,
+                'order'     => 5,
+                'title'     => 'Permission',
+                'icon'      => 'fa-ban',
+                'uri'       => 'auth/permissions',
+            ],
+            [
+                'parent_id' => 2,
+                'order'     => 6,
+                'title'     => 'Menu',
+                'icon'      => 'fa-bars',
+                'uri'       => 'auth/menu',
+            ],
+            [
+                'parent_id' => 2,
+                'order'     => 7,
+                'title'     => 'Operation log',
+                'icon'      => 'fa-history',
+                'uri'       => 'auth/logs',
+            ],
+        ]);
+
+        // add role to menu.
+        //Menu::find(2)->roles()->save(Role::first());
+        
+    }
+}
